a1603edc3d267b991c4251de3014ac89e9ce1074
JoaoGuimaraes-11 <jonyjocas11@gmail.com>
2024-12-06 02:02:53 -0300

    Atualizacoes

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
+<?php include('config.php'); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="estilo/style.css">
+    <link <?php echo INCLUDE_PATH; ?>  rel="stylesheet" href="estilo/style.css">
     <title>Projeto 01</title>
 </head>
 <body>
+   
     <header>
         <div class="center">
             <div class="logo left"><!--Logo marca-->
-                Studio Refart
+                <a href="/">Marcelo</a>
             </div>
             <nav class="desktop right ">
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Serviços</a></li>
-                    <li><a href="#">Contato</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
                 </ul>
             </nav>
             <nav class="mobile right">
@@ -26,10 +27,11 @@
                 <i class="fa-solid fa-bars"></i>
                 </div>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Serviços</a></li>
-                    <li><a href="#">Contato</a></li>
+                <li><a href="#">Home</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>sobre">Sobre</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?> servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH; ?>contato">Contato</a></li>
+                </ul>
                 </ul>
             </nav>
             <div class="clear"></div><!--Clear-->
@@ -51,15 +53,14 @@
     <section class="descricao-autor">
         <div class="center">
         <div class="w50 left">
-         <h2 class="teste">Joao Vitor</h2>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum fuga facilis voluptates quod excepturi quas vel odio velit illo, sunt magni perferendis laboriosam hic eius minima suscipit ipsum recusandae.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum fuga facilis voluptates quod excepturi quas vel odio velit illo, sunt magni perferendis laboriosam hic eius minima suscipit ipsum recusandae.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum fuga facilis voluptates quod excepturi quas vel odio velit illo, sunt magni perferendis laboriosam hic eius minima suscipit ipsum recusandae.</p>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus eum fuga facilis voluptates quod excepturi quas vel odio velit illo, sunt magni perferendis laboriosam hic eius minima suscipit ipsum recusandae.</p>
+         <h2 class="teste">Marcelo Guimarães</h2>
+         <p>Minha jornada no design gráfico começou em um momento de grande transformação pessoal. Durante anos, trabalhei como entregador, uma profissão que, embora me proporcionasse um sustento, não preenchia minhas expectativas e sonhos. Eu sabia que queria algo mais para minha vida, algo que me permitisse expressar minha criatividade e fazer a diferença de forma mais significativa.</p>
+         <p>Foi então que decidi mudar de rumo e comecei meus estudos em design gráfico. A decisão de estudar algo completamente novo e desafiador não foi fácil, mas eu sabia que era a chave para a mudança que tanto almejava. No começo, enfrentei diversas dificuldades, desde o domínio de ferramentas e conceitos até o entendimento do processo criativo e da importância de cada detalhe no design.</p>
+         <p>Com o tempo, fui me apaixonando cada vez mais pelo design gráfico. Aprendi que a comunicação visual tem o poder de transformar ideias em imagens que impactam, que um bom design não é apenas bonito, mas também funcional e estratégico. Cada projeto se tornou uma oportunidade de aplicar o que estava aprendendo e testar minhas habilidades.</p>
         </div><!--W50-->
         <div class="w50 left responsive">
             <!--Pegar imagem depois-->
-            <img class="autor" src="images/foto.jpg" alt="">
+            <img class="autor" src="<?php INCLUDE_PATH?>images/foto.jpg" alt="">
         </div><!--W50-->
         <div class="clear"></div>
         </div><!--Center-->
@@ -69,19 +70,19 @@
         <div class="center">
         <h2 class="title">Especialidades</h2>
             <div class="w33 left box-especialidade">
-                <h3><i class="fa-brands fa-css3"></i></h3>
-                <h4>CSS3</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic saepe, itaque eum, architecto ab perspiciatis earum beatae a, quia numquam officiis eaque doloremque aliquid aliquam. Vel culpa numquam mollitia voluptas.</p>
+                <h3><i class="fa-solid fa-desktop"></i></h3>
+                <h4>SOCIAL MEDIA</h4>
+                <p style="text-align: left;">Meu processo envolve entender as necessidades e o público-alvo de cada cliente, criando designs atraentes e eficazes que gerem engajamento e reforcem a mensagem da marca. Além disso, busco sempre estar atualizado com as tendências visuais e as melhores práticas de cada rede social, garantindo que os designs sejam otimizados para cada plataforma.</p>
             </div><!--Box-especialidade-->
             <div class="w33 left box-especialidade">
-                <h3><i class="fa-brands fa-html5"></i></h3>
-                <h4>HTML5</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic saepe, itaque eum, architecto ab perspiciatis earum beatae a, quia numquam officiis eaque doloremque aliquid aliquam. Vel culpa numquam mollitia voluptas.</p>
+                <h3><i class="fa-solid fa-pen-nib"></i></h3>
+                <h4>PHOTOSHOP</h4>
+                <p style="text-align: left;">Como especialista em Photoshop, crio designs gráficos impactantes, manipulando imagens e aplicando técnicas avançadas de edição. Transformo ideias em visuais de alta qualidade, desde a criação de conteúdos para redes sociais até projetos publicitários. Minha habilidade em Photoshop permite oferecer soluções criativas e detalhadas para atender às necessidades de meus clientes.</p>
             </div><!--Box-especialidade-->
             <div class="w33 left box-especialidade">
-                <h3><i class="fa-brands fa-js"></i></h3>
-                <h4>JAVASCRIPT</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic saepe, itaque eum, architecto ab perspiciatis earum beatae a, quia numquam officiis eaque doloremque aliquid aliquam. Vel culpa numquam mollitia voluptas.</p>
+                <h3><i class="fa-brands fa-figma"></i></h3>
+                <h4>FIGMA</h4>
+                <p style="text-align: left;">Como especialista em Figma, crio designs digitais interativos e interfaces de usuário (UI) funcionais e atraentes. Desenvolvo protótipos, wireframes e layouts, focando em uma experiência de usuário (UX) intuitiva. Minha experiência no Figma permite entregar projetos colaborativos, ágeis e otimizados para diversas plataformas, atendendo às necessidades dos clientes com precisão.</p>
             </div><!--Box-especialidade-->
             <div class="clear"></div>
         </div><!--Center-->
@@ -123,8 +124,8 @@
             <p>todos os direitos reservados</p>
         </div><!--Center-->
     </footer>
-    <script src="https://kit.fontawesome.com/9aff4d7fb2.js" crossorigin="anonymous"></script>
-    <script src="js/jquery-3.7.1.js"></script>
-    <script src="js/scripts.js"></script>
+    <script <?php echo INCLUDE_PATH; ?>  src="https://kit.fontawesome.com/9aff4d7fb2.js" crossorigin="anonymous"></script>
+    <script <?php echo INCLUDE_PATH; ?> src="js/jquery-3.7.1.js"></script>
+    <script <?php echo INCLUDE_PATH; ?> src="js/scripts.js"></script>
 </body>
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(function(){
     //Aqui vai todo nosso código de JavaScript
+    $('nav.mobile').click(function(){
+        //O que vai acontecer quando clicarmos na nav.mobile!
+        var listaMenu = $('nav.mobile ul');
+        // listaMenu.slideToggle();//abrir ou fechar o menu
+
+        //<i class="fa-solid fa-x"></i>
+
+        if(listaMenu.is(':hidden') == true){
+            var icone = $('.botao-menu-mobile').find('i');
+            icone.removeClass('fa-bars');
+            icone.addClass('fa-times');
+            listaMenu.slideToggle();
+        }
+        else{
+            var icone = $('.botao-menu-mobile').find('i');
+            icone.removeClass('fa-times');
+            icone.addClass('fa-bars');
+            listaMenu.slideToggle();
+         }
+    })
+
 })

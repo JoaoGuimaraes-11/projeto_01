202b87946b059e764966bad0f030b153820cc07a
JoaoGuimaraes-12 <jonyjocas11@gmail.com>
2024-12-07 02:20:11 -0300

    pagina 404

--- index.php
+++ index.php
@@ -38,88 +38,21 @@
         </div><!--Center-->
     </header>
 
-    <section class="banner-principal">
-        <div class="overlay"></div>
-        <div class="center">
-        <form action="#">
-            <h2>Qual o seu melhor e-mail</h2>
-            <input type="email" name="email" required>
-            <input type="submit" name="acao" value="Cadastrar">
-        </form>
-        </div><!--Center-->
-        <div class="clear"></div>
-    </section> <!--Banner principal-->
-    
-    <section class="descricao-autor">
-        <div class="center">
-        <div class="w50 left">
-         <h2 class="teste">Marcelo Guimarães</h2>
-         <p>Minha jornada no design gráfico começou em um momento de grande transformação pessoal. Durante anos, trabalhei como entregador, uma profissão que, embora me proporcionasse um sustento, não preenchia minhas expectativas e sonhos. Eu sabia que queria algo mais para minha vida, algo que me permitisse expressar minha criatividade e fazer a diferença de forma mais significativa.</p>
-         <p>Foi então que decidi mudar de rumo e comecei meus estudos em design gráfico. A decisão de estudar algo completamente novo e desafiador não foi fácil, mas eu sabia que era a chave para a mudança que tanto almejava. No começo, enfrentei diversas dificuldades, desde o domínio de ferramentas e conceitos até o entendimento do processo criativo e da importância de cada detalhe no design.</p>
-         <p>Com o tempo, fui me apaixonando cada vez mais pelo design gráfico. Aprendi que a comunicação visual tem o poder de transformar ideias em imagens que impactam, que um bom design não é apenas bonito, mas também funcional e estratégico. Cada projeto se tornou uma oportunidade de aplicar o que estava aprendendo e testar minhas habilidades.</p>
-        </div><!--W50-->
-        <div class="w50 left responsive">
-            <!--Pegar imagem depois-->
-            <img class="autor" src="<?php INCLUDE_PATH?>images/foto.jpg" alt="">
-        </div><!--W50-->
-        <div class="clear"></div>
-        </div><!--Center-->
-    </section><!--Descricao autor-->
+    <?php 
+        $url = isset($_GET['url']) ? $_GET['url'] : 'home';
+        
+        if(file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }
+        else{
+            //podemos fazer o que quiser pois a pagina nao existe
+            $pagina404 = true;
+            include('pages/404.php');
+        }
+    ?>
+     
 
-    <section class="especialidades">
-        <div class="center">
-        <h2 class="title">Especialidades</h2>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fa-solid fa-desktop"></i></h3>
-                <h4>SOCIAL MEDIA</h4>
-                <p style="text-align: left;">Meu processo envolve entender as necessidades e o público-alvo de cada cliente, criando designs atraentes e eficazes que gerem engajamento e reforcem a mensagem da marca. Além disso, busco sempre estar atualizado com as tendências visuais e as melhores práticas de cada rede social, garantindo que os designs sejam otimizados para cada plataforma.</p>
-            </div><!--Box-especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fa-solid fa-pen-nib"></i></h3>
-                <h4>PHOTOSHOP</h4>
-                <p style="text-align: left;">Como especialista em Photoshop, crio designs gráficos impactantes, manipulando imagens e aplicando técnicas avançadas de edição. Transformo ideias em visuais de alta qualidade, desde a criação de conteúdos para redes sociais até projetos publicitários. Minha habilidade em Photoshop permite oferecer soluções criativas e detalhadas para atender às necessidades de meus clientes.</p>
-            </div><!--Box-especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fa-brands fa-figma"></i></h3>
-                <h4>FIGMA</h4>
-                <p style="text-align: left;">Como especialista em Figma, crio designs digitais interativos e interfaces de usuário (UI) funcionais e atraentes. Desenvolvo protótipos, wireframes e layouts, focando em uma experiência de usuário (UX) intuitiva. Minha experiência no Figma permite entregar projetos colaborativos, ágeis e otimizados para diversas plataformas, atendendo às necessidades dos clientes com precisão.</p>
-            </div><!--Box-especialidade-->
-            <div class="clear"></div>
-        </div><!--Center-->
-    </section><!--Especialidades-->
-
-    <section class="extras">
-        <div class="center">
-            <div class="w50 left depoimentos-container">
-                <h2 class="title">Depoimentos Dos Nossos Clientes</h2>
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia autem repellendus, iste incidunt commodi ullam tempore, reiciendis officiis nostrum, expedita maxime labore magnam sint voluptates blanditiis eveniet inventore ab dolores.</p>
-                    <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-single-->
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia autem repellendus, iste incidunt commodi ullam tempore, reiciendis officiis nostrum, expedita maxime labore magnam sint voluptates blanditiis eveniet inventore ab dolores.</p>
-                    <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-single-->
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia autem repellendus, iste incidunt commodi ullam tempore, reiciendis officiis nostrum, expedita maxime labore magnam sint voluptates blanditiis eveniet inventore ab dolores.</p>
-                    <p class="nome-autor">Lorem Ipsum</p>
-                </div><!--depoimento-single-->
-            </div><!--W50-->
-            <div class="w50 left servicos-container">
-                <h2 class="title">Serviços</h2>
-                <div class="servicos">
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam qui commodi odio aperiam non sequi. Fuga ea est, mollitia atque temporibus, deserunt animi accusantium quasi eligendi, cum impedit provident esse!</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam qui commodi odio aperiam non sequi. Fuga ea est, mollitia atque temporibus, deserunt animi accusantium quasi eligendi, cum impedit provident esse!</li>
-                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam qui commodi odio aperiam non sequi. Fuga ea est, mollitia atque temporibus, deserunt animi accusantium quasi eligendi, cum impedit provident esse!</li>
-                    </ul>
-                </div>
-            </div><!--W50-->
-            <div class="clear"></div>
-        </div><!--Center-->
-    </section><!--Extras-->
-
-    <footer>
+    <footer <?php if(isset($pagina404) && $pagina404 == true ) echo 'class="fixed";' ?>>
         <div class="center">
             <p>todos os direitos reservados</p>
         </div><!--Center-->
---
+++ pages/404.php
@@ -0,0 +1,8 @@
+<section class="erro-404">
+    <div class="center">
+        <div class="wraper-404">
+            <h2>erro 404</h2>
+            <p>Deseja voltar para <a href="<?php echo INCLUDE_PATH?>">Página Inicial</a></p>
+        </div><!--Wrapper-404-->
+    </div>
+</section>

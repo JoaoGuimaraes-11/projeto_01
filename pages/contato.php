0c2e7cf1eeb7f0a685e76a5715d44bd26fd482b4
JoaoGuimaraes-12 <jonyjocas11@gmail.com>
2024-12-09 22:39:41 -0300

    -a

---
+++ pages/contato.php
@@ -0,0 +1,10 @@
+<section class="contato-container">
+    <div class="center">
+        <form class ="form-container" action="#">
+            <input type="text" name="nome" id="#"  placeholder = "Digite o seu nome">
+            <input type="text" name="email" id="#" placeholder = "Digite o seu e-mail">
+            <textarea name="textarea" id="#"></textarea>
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+</section>
